5358796f259ebc8670e3716a987d661eb1b6fc15
olumidayy <niolumi4eva@gmail.com>
2021-08-08 22:13:23 +0100

    first commit

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -4,8 +4,8 @@ import Logger from './logger';
 
 export default async ({ expressApp }) => {
   const mongoConnection = await mongooseLoader();
-  Logger.info('✌️ DB loaded and connected!');
+  Logger.info('- DB loaded and connected!');
 
   await expressLoader({ app: expressApp });
-  Logger.info('✌️ Express loaded');
+  Logger.info('- Express loaded');
 };
